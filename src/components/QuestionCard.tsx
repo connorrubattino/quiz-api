d5033c4a4c3d9c1d482348622805773255691fb2
Connor Rubattino <connor.rubattino@gmail.com>
2024-04-22 10:05:59 -0700

    reveal questions with button

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,6 +2,7 @@ import { QuestionType } from "../types";
 import Card from "react-bootstrap/Card";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState } from "react";
 
 type QuestionCardProps = {
     question:QuestionType
@@ -9,14 +10,20 @@ type QuestionCardProps = {
 
 
 export default function QuestionCard({ question }: QuestionCardProps) {
+
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  const handleClick = () => {
+    setShowAnswer(!showAnswer)
+  };
+
   return (
     <Card className='m-5'>
       <Card.Header as="h5">Question</Card.Header>
       <Card.Body>
         <Card.Title>{ question.question }</Card.Title>
         <Form.Label>Answer:</Form.Label>
-        <Form.Control name='answer' type="text" placeholder="Answer" />
-        <Button className='m-2' variant="success">Submit</Button>
+        <Button className='m-2 w-100' variant={showAnswer ? "warning" : "primary"} onClick={handleClick}>{showAnswer ? `${question.answer}` : "Reveal Answer"}</Button>
       </Card.Body>
     </Card>
   )
